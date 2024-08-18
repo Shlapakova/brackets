2681667eec5ea6791493566e198ccb0c039c812c
Shlapakova <shlapakova.kristina@mail.ru>
2024-08-18 21:35:04 +0200

    unit: upload project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketPairs = new Map(bracketsConfig);
+  const bracketStack = [];
+  for (const i of str) {
+    if (bracketPairs.has(i)) {
+      if (bracketPairs.get(i) === i) {
+        if (bracketStack.length > 0 && bracketStack[bracketStack.length - 1] === i) {
+          bracketStack.pop();
+        } else {
+          bracketStack.push(i);
+        }
+      } else {
+    bracketStack.push(i);
+      }
+    } else {
+      const last = bracketStack.pop();
+      if (bracketPairs.get(last) !== i) {
+        return false;
+}
+ }
+ }
+  return bracketStack.length === 0;
 }
